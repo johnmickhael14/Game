44a7ad322da111304c7532b5cb35cb0e74f72765
johnmickhael14 <johnmickhael14@gmail.com>
2026-02-01 16:43:29 +0800

    Fade in Main Menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var button_type=null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,7 +12,10 @@ func _process(delta: float) -> void:
 
 #start
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	button_type="start"
+	$fade_transition.show()
+	$fade_transition/fade_timer.start()
+	$fade_transition/AnimationPlayer.play("Fade in")
 
 #Quit
 func _on_quit_game_pressed() -> void:
@@ -20,8 +23,16 @@ func _on_quit_game_pressed() -> void:
 
 #Stage Select
 func _on_stage_select_pressed() -> void:
-	pass # Replace with function body.
-
+	button_type="option"
+	$fade_transition.show()
+	$fade_transition/fade_timer.start()
+	$fade_transition/AnimationPlayer.play("Fade in")
+	$Labels/AnimationPlayer.play("fade_in")
 #Reset Data
 func _on_reset_data_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type=="start":
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
