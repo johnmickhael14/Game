04453e943227a998fb5fbef54face51addda985e
johnmickhael14 <johnmickhael14@gmail.com>
2026-02-02 21:54:57 +0800

    enemy 1

---
+++ scenes/enemy.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED=60
+# Called when the node enters the scene tree for the first time.
+var direction=1
+@onready var ray_left: RayCast2D = $RayLeft
+@onready var ray_right: RayCast2D = $RayRight
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_left.is_colliding():
+		direction=1
+	if ray_right.is_colliding():
+		direction=-1
+	position.x+=direction*SPEED*delta
