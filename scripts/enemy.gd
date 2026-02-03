0d698aa7ad571a4c121a891588fadbf2ee516603
johnmickhael14 <johnmickhael14@gmail.com>
2026-02-03 18:22:14 +0800

    Enemy
    Added Enemy and a flag sprite

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED=60
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +14,8 @@ var direction=1
 func _process(delta: float) -> void:
 	if ray_left.is_colliding():
 		direction=1
+		animated_sprite_2d.flip_h=false
 	if ray_right.is_colliding():
 		direction=-1
+		animated_sprite_2d.flip_h=true
 	position.x+=direction*SPEED*delta
