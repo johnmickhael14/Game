a62a6341afde72e92e3177950119ed5da84537ad
johnmickhael14 <johnmickhael14@gmail.com>
2026-03-24 20:39:03 +0800

    TELEPORT FINALLY WORKED

--- scripts/teleport_try.gd
+++ scripts/teleport_try.gd
@@ -3,4 +3,4 @@ var PlayerController: PlayerController
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerController:
-		print("hello")
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
